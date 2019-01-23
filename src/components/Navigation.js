51b3c64299f5ff63809795a8cbb3ef0566f93a0d
Takatoshi Wada <fueledbytaching@gmail.com>
2019-01-23 17:28:53 +0900

    added material-ui and some cleanup

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,73 @@
-import React, { Component } from 'react'
-import { Link } from 'gatsby'
-export default class Navigation extends Component {
-  render() {
-    return (
-      <div>
-        <ul>
-          <Link to={`/`}>HOME</Link>
-        </ul>
-      </div>
-    )
-  }
+// import React from 'react'
+// import PropTypes from 'prop-types'
+// import { withStyles } from '@material-ui/core/styles'
+// import AppBar from '@material-ui/core/AppBar'
+// import Toolbar from '@material-ui/core/Toolbar'
+// import { Navlink } from './styles/Allstyle'
+
+// const styles = {
+//   root: {
+//     flexGrow: 1,
+//   },
+// }
+
+// function SimpleAppBar(props) {
+//   const { classes } = props
+
+//   return (
+//     <div className={classes.root}>
+//       <AppBar position="static" color="default">
+//         <Toolbar>
+//           <Navlink to="/">Home</Navlink>
+//           <Navlink to="/blog">blog</Navlink>
+//         </Toolbar>
+//       </AppBar>
+//     </div>
+//   )
+// }
+
+// SimpleAppBar.propTypes = {
+//   classes: PropTypes.object.isRequired,
+// }
+
+// export default withStyles(styles)(SimpleAppBar)
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import { Navlink } from './styles/Allstyle'
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
 }
+
+function ButtonAppBar(props) {
+  const { classes } = props
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Navlink to="/">Home</Navlink>
+          <Navlink to="/blog">blog</Navlink>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+ButtonAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(ButtonAppBar)
