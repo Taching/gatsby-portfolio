5b8d9b2426b1e52447b0ee87ee7c0ae11fc499d0
Takatoshi Wada <fueledbytaching@gmail.com>
2019-01-23 18:53:22 +0900

    added global new colors and styles to navigation

--- src/components/styles/componentstyle.js
+++ src/components/styles/componentstyle.js
@@ -2,8 +2,16 @@ import styled from 'styled-components'
 import { Link } from 'gatsby'
 import * as color from './colors'
 
+export const NavStyle = styled.div`
+  display: flex;
+`
 export const Navlink = styled(Link)`
   color: ${color.white};
   text-decoration: none;
   margin: 10px 10px;
+  box-shadow: none;
+`
+export const Logo = styled.span`
+  color: ${color.white};
+  font-family: 'Courier New', Courier, monospace;
 `
